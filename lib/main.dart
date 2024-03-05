f633e975810108aca78fa5e2f0aac622572a557c
Wrth1 <billelimgg@gmail.com>
2024-03-05 20:53:38 +0700

    add delete button

--- lib/main.dart
+++ lib/main.dart
@@ -127,11 +127,16 @@ class _NotepadHomePageState extends State<NotepadHomePage> {
   }
 }
 
-class NotesListPage extends StatelessWidget {
+class NotesListPage extends StatefulWidget {
   final List<String> notes;
 
-  const NotesListPage({super.key, required this.notes});
+  const NotesListPage({Key? key, required this.notes}) : super(key: key);
 
+  @override
+  _NotesListPageState createState() => _NotesListPageState();
+}
+
+class _NotesListPageState extends State<NotesListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -140,10 +145,17 @@ class NotesListPage extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 227, 179, 235),
       ),
       body: ListView.builder(
-        itemCount: notes.length,
+        itemCount: widget.notes.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(notes[index]),
+            title: Text(widget.notes[index]),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                // Handle delete button press
+                _deleteNoteAtIndex(context, index);
+              },
+            ),
             onTap: () {
               Navigator.pop(context, index);
             },
@@ -151,5 +163,39 @@ class NotesListPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _deleteNoteAtIndex(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Note'),
+          content: const Text('Are you sure you want to delete this note?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _removeNoteAtIndex(index);
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _removeNoteAtIndex(int index) {
+    setState(() {
+      // Remove the note at the specified index from the notes list
+      widget.notes.removeAt(index);
+    });
   }
 }
